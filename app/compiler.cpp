91e7c4ed5e1aab637e30a75466b942343bf46bcb
lreuter2020 <lreuter2020@gmail.com>
2023-09-02 21:18:45 -0700

    created app repo to hold code for compiler

---
+++ app/compiler.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+#include "../src/lexer/lexer.hpp"
+#include "../src/errors/error.hpp"
+
+int main(int argc, char **argv)
+{
+    std::cout << "Hello World!" << std::endl;
+    return 0;
+}
