89aff60fb4dfb2e5b8de9b65268d6891abd74474
lreuter2020 <lreuter2020@gmail.com>
2023-09-04 20:49:18 -0700

    updated compiler to call parser

--- app/compiler.cpp
+++ app/compiler.cpp
@@ -1,18 +1,32 @@
 #include <iostream>
+#include <sstream>
+#include <fstream>
 
 #include "../src/lexer/lexer.hpp"
 #include "../src/errors/error.hpp"
+#include "../src/parser/parser.hpp"
 
 int main(int argc, char **argv)
 {
-    // if (argc < 2)
-    // {
-    //     std::cout << "Insufficient arguments passed" << std::endl;
-    //     return EXIT_FAILURE;
-    // }
+    if (argc < 2)
+    {
+        std::cout << "Insufficient arguments passed" << std::endl;
+        return EXIT_FAILURE;
+    }
 
-    std::cout << "Tokens (string)" << std::endl;
-    Lexer lexer = Lexer("let x = 5");
+    std::string contents;
+    {
+        std::stringstream contents_stream;
+        std::fstream input(argv[1], std::ios::in);
+        contents_stream << input.rdbuf();
+        contents = contents_stream.str();
+    }
+
+    Lexer lexer(contents);
+    std::vector<Token> tokens = lexer.tokenize();
+
+    Parser parser(tokens);
+    parser.parse();
     
     return 0;
 }
