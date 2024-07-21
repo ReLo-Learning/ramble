d5e24a3a2025c49d3f36601b367aff6b4150b740
lreuter2020 <lreuter2020@gmail.com>
2024-07-21 16:39:10 -0700

    added values to TokenKind

--- src/lexer/kind.cpp
+++ src/lexer/kind.cpp
@@ -6,17 +6,24 @@ std::map<Kind, std::string> TokenKind = {
 
     /*Literals*/
     {IDENT, "IDENT"},
-    {INT, "INT"},
-    {FLOAT, "FLOAT"},
+    {INT_LIT, "INT_LIT"},
+    {FLOAT_LIT, "FLOAT_LIT"},
 
     /*Symbols*/
     {EQ, "="},
+    {ADD, "+"},
+    {SUB, "-"},
+    {MUL, "*"},
+    {DIV, "/"},
+    {MOD, "%"},
     {SEMI, ";"},
 
     {INCR, "++"},
 
     /*Keywords*/
     {LET, "LET"},
+    {INT, "INT"},
+    {FLOAT, "FLOAT"},
 };
 
 std::string getType(Kind kind)
