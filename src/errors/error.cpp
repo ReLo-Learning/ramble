4f234b2cc0ef3ed769214c02fee3c9533af1a580
lreuter2020 <lreuter2020@gmail.com>
2025-01-28 20:59:41 -0800

    adjusted error class for better printing
    still in work, want it to feel like other languages where it gives context, may adjust the error function to be incorporated into each class like Parser and Lexer

--- src/errors/error.cpp
+++ src/errors/error.cpp
@@ -1,1 +1,13 @@
 #include "error.hpp"
+
+std::map<ErrorType, std::string> ErrorTypeString = {
+    {LEXER, "LEXER"},
+    {PARSER, "PARSER"},
+};
+
+std::string Error::str()
+{
+    std::stringstream ss;
+
+    return ss.str();
+}
--- src/errors/error.hpp
+++ src/errors/error.hpp
@@ -3,13 +3,27 @@
 #ifndef ERROR_HPP
 #define ERROR_HPP
 
+#include <sstream>
 #include <string>
+#include <map>
+
+// enum to specify different types of common errors (Lexer, Parser, Codegen, etc)
+enum ErrorType 
+{
+    LEXER,
+    PARSER,
+
+};
+
 class Error
 {
 public:
     std::string msg;
+    std::string filepath;
+    unsigned int lineNum;
+    unsigned int colNum;
 
-    virtual ~Error() = default;
+    std::string str();
 };
 
 #endif
