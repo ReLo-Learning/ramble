e46bc77ab32bcd7786249a882e14888ff1842da1
Logan Reuter <lreuter2020@gmail.com>
2025-02-03 17:04:25 -0800

    revised how the compiler operates

--- app/compiler.cpp
+++ app/compiler.cpp
@@ -2,10 +2,12 @@
 #include <sstream>
 #include <fstream>
 
-#include "../src/lexer/lexer.hpp"
-#include "../src/errors/error.hpp"
-#include "../src/parser/parser.hpp"
-#include "../src/hir/hir.hpp"
+#include "../src/module/module.hpp"
+
+// #include "../src/lexer/lexer.hpp"
+// #include "../src/errors/error.hpp"
+// #include "../src/parser/parser.hpp"
+// #include "../src/hir/hir.hpp"
 
 int main(int argc, char **argv)
 {
@@ -23,15 +25,19 @@ int main(int argc, char **argv)
     //     contents = contents_stream.str();
     // }
 
-    Lexer lexer(argv[1]);
-    std::vector<Token> tokens = lexer.tokenize();
-    lexer.print();
+    module::Module program(argv[1]);
 
-    Parser parser(tokens);
-    std::unique_ptr<AST::Program> program = parser.parse();
-    program->print();
+    // Lexer lexer(argv[1]);
+    // std::vector<Token> tokens = lexer.tokenize();
+    // lexer.print();
 
-    HIR::Lower(&*program);
+    // Parser parser(tokens, lexer.getFileSet());
+    // std::unique_ptr<AST::Program> program = parser.parse();
+    // program->print();
+
+    // std::unique_ptr<HIR::Walker> ir = HIR::Lower(&*program);
+    // ir->print();
+
     // Codegen::generate(std::move(program));
 
     return 0;
